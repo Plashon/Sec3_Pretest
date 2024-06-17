316acec9f114a3a1f33e88fc05db2a29ec0eb5f0
Poobate Nimnuan <111554614+Plashon@users.noreply.github.com>
2024-06-17 15:36:15 +0700

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <h1 className="title" id="title">
+          Grab Restarant
+        </h1>
+        <div className="search">
+          <input type="text" placeholder="Search..." size={30}></input>
+        </div>
+        <div className="menu" id="menu">
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+          <div className="card" id="c01">
+            <img
+              alt="Order ครัวอัปษร - ถนนดินสอ"
+              class="realImage___2TyNE show___3oA6B"
+              src="https://food-cms.grab.com/compressed_webp/merchants/THGFIST0000022t/hero/2f024f5dd1c64e7aaf45417e2ebd11e9_1711446459727933226.webp"
+            ></img>
+
+            <div className="menu_title">
+              <h2 className="menu_name">Order ครัวอัปษร - ถนนดินสอ</h2>
+              <h3> อาหารทะเล, อาหารตามสั่ง, อาหารเส้น</h3>
+            </div>
+            <input id="btn" type="button" value="visit" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
